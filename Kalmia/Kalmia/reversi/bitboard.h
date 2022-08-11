ca745b6d7eaee6fa65958673d3d388382b002150
kalmia36892@gmail.com <kalmia36892@gmail.com>
2022-08-11 13:47:50 +0900

    static_initializerの追加. Bitboardクラスに新たなメンバ関数を追加.

--- Kalmia/Kalmia/reversi/bitboard.h
+++ Kalmia/Kalmia/reversi/bitboard.h
@@ -1,13 +1,15 @@
 #pragma once
 
 #include "../common.h"
+#include "../utils/bitmanip.h"
+
 #include "types.h"
 #include "mobility.h"
 #include "flip.h"
 
 namespace reversi
 {
-	class BitBoard
+	class Bitboard
 	{
 	private:
 		uint64_t _player;
@@ -18,9 +20,27 @@ namespace reversi
 		inline uint64_t opponent() { return this->_opponent; }
 		inline uint64_t discs() { return this->_player | this->_opponent; }
 		inline uint64_t empties() { return ~discs(); }
+		inline int32_t player_disc_count() { return std::popcount(this->_player); }
+		inline int32_t opponent_disc_count() { return std::popcount(this->_opponent); }
+		inline int32_t disc_count() { return std::popcount(discs()); }
+		inline int32_t empty_count() { return std::popcount(empties()); }
 		inline uint64_t calc_player_mobility() { return calc_mobility(this->_player, this->_opponent); }
 		inline uint64_t calc_opponent_mobility(){return calc_mobility(this->_opponent, this->_player);}
 		inline uint64_t calc_flipped_discs(BoardCoordinate& coord) { return reversi::calc_flipped_discs(this->_player, this->_opponent, coord); }
+
+		inline void update(BoardCoordinate& coord, uint64_t& flipped)
+		{
+			this->_player |= (COORD_TO_BIT[coord] | flipped);
+			this->_opponent ^= flipped;
+			swap();
+		}
+
+		inline void undo(BoardCoordinate& coord, uint64_t& flipped)
+		{
+			swap();
+			this->_player ^= (COORD_TO_BIT[coord] | flipped);
+			this->_opponent ^= flipped;
+		}
 
 		inline void swap()
 		{
--- Kalmia/Kalmia/reversi/position.h
+++ Kalmia/Kalmia/reversi/position.h
@@ -2,6 +2,7 @@
 
 #include "../common.h"
 #include "../config.h"
+#include "../utils/static_initializer.h"
 #include "constant.h"
 #include "types.h"
 #include "bitboard.h"
@@ -10,6 +11,16 @@ namespace reversi
 {
 	class Position
 	{
+	private:
+		// Rankというのはチェス用語で, 盤面の水平方向のラインを意味する.
+		static constexpr int HASH_RANK_LEN_0 = 16;
+		static constexpr int HASH_RANK_LEN_1 = 256;
+		static ConstantArray<uint64_t, HASH_RANK_LEN_0 * HASH_RANK_LEN_1> HASH_RANK;
 
+		Bitboard _bitboard;
+		SquareState _side_to_move;
+
+	public:
+		static void init();
 	};
 }
