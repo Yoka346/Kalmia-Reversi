86fb696ca13d79ca013667402b5624905cb989b4
kalmia36892@gmail.com <kalmia36892@gmail.com>
2022-08-31 17:27:58 +0900

    GameTimerをカナダ方式に対応.

--- Kalmia/Kalmia/utils/game_timer.cpp
+++ Kalmia/Kalmia/utils/game_timer.cpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "game_timer.h"
+
+#include <cmath>
+
 #include "../utils/exception.h"
 
 using namespace utils;
@@ -10,6 +13,7 @@ void GameTimer::start()
 {
 	if (is_ticking())
 		throw utils::invalid_operation("timer is ticking.");
+
 	if (timeout())
 		throw utils::invalid_operation("timeout.");
 
@@ -23,16 +27,22 @@ void GameTimer::stop()
 		throw utils::invalid_operation("timer is not ticking.");
 
 	auto ellapsed = duration_cast<milliseconds>(high_resolution_clock::now() - this->check_point);
-	this->_is_ticking = false;
 	this->_time_left_ms -= ellapsed;
+	this->_is_ticking = false;
+
 	if (this->_time_left_ms < milliseconds::zero())
 	{
 		this->_time_left_ms = milliseconds::zero();
 		this->_timeout = true;
+		return;
 	}
 
 	if (this->_time_left_ms < this->_byoyomi_ms)
-		this->_time_left_ms = this->_byoyomi_ms;
+		if (--this->_byoyomi_stones_left == 0)
+		{
+			this->_byoyomi_stones_left = this->_byoyomi_stones;
+			this->_time_left_ms = this->_byoyomi_ms;
+		}
 	
 	this->_time_left_ms += this->_increment_ms;
 }
@@ -43,12 +53,26 @@ void GameTimer::reset()
 	this->_time_left_ms = this->_main_time_ms + this->_byoyomi_ms;
 }
 
+template<bool INCLUDE_BYOYOMI>
 milliseconds GameTimer::time_left_ms()
 {
 	if (!is_ticking())
-		return milliseconds::zero();
+		this->_time_left_ms;
 
 	auto prev_check_point = this->check_point;
 	this->check_point = high_resolution_clock::now();
-	return this->_main_time_ms -= duration_cast<milliseconds>(this->check_point - prev_check_point);
+	this->_time_left_ms -= duration_cast<milliseconds>(this->check_point - prev_check_point);
+
+	if constexpr (INCLUDE_BYOYOMI)
+		return std::max(milliseconds::zero(), this->_time_left_ms);
+
+	return std::max(milliseconds::zero(), this->_time_left_ms - this->_byoyomi_ms);
+}
+
+milliseconds GameTimer::byoyomi_left_ms()
+{
+	if (time_left_ms<false>() != milliseconds::zero())
+		return this->_byoyomi_ms;
+	else
+		return this->_time_left_ms;
 }
--- Kalmia/Kalmia/utils/game_timer.h
+++ Kalmia/Kalmia/utils/game_timer.h
@@ -8,28 +8,36 @@ namespace utils
 	{
 	private:
 		std::chrono::milliseconds _main_time_ms;
-		std::chrono::milliseconds _byoyomi_ms;	// 秒読み. メインの持ち時間が切れたときに, 1手ごとに設けられる時間. 日本のゲームでよく用いられる.
+		std::chrono::milliseconds _byoyomi_ms;	// 秒読み. メインの持ち時間が切れたときに, _byoyomi_stones手ごとに設けられる時間. 日本のゲームでよく用いられる.
+		int32_t _byoyomi_stones;	 
 		std::chrono::milliseconds _increment_ms;	// フィッシャールールにおける1手ごとの持ち時間の加算分
 		std::chrono::milliseconds _time_left_ms;	// 持ち時間の残り + 秒読み
+		int32_t _byoyomi_stones_left;
 		bool _is_ticking;
 		bool _timeout;
 		std::chrono::high_resolution_clock::time_point check_point;
 
 	public:
-		GameTimer() : GameTimer(std::chrono::milliseconds::zero(), std::chrono::milliseconds::zero(), std::chrono::milliseconds::zero()) { ; }
-		GameTimer(std::chrono::milliseconds main_time_ms, std::chrono::milliseconds byoyomi_ms, std::chrono::milliseconds inc_ms)
-			: _main_time_ms(main_time_ms), _byoyomi_ms(byoyomi_ms), _increment_ms(inc_ms), 
+		GameTimer() : GameTimer(std::chrono::milliseconds::zero(), std::chrono::milliseconds::zero(), 1, std::chrono::milliseconds::zero()) { ; }
+		GameTimer(std::chrono::milliseconds main_time_ms, std::chrono::milliseconds byoyomi_ms, int32_t byoyomi_stones, std::chrono::milliseconds inc_ms)
+			: _main_time_ms(main_time_ms), _byoyomi_ms(byoyomi_ms), _byoyomi_stones(byoyomi_stones), _increment_ms(inc_ms),
 			  _time_left_ms(main_time_ms + byoyomi_ms), _is_ticking(false), _timeout(false) { ; }
 
-		inline std::chrono::milliseconds main_time_ms() { return this->_main_time_ms; }
-		inline std::chrono::milliseconds byoyomi_ms() { return this->_byoyomi_ms; }
-		inline std::chrono::milliseconds increment_ms() { return this->_increment_ms; }
-		inline bool is_ticking() { return this->_is_ticking; }
-		inline bool timeout() { return this->_timeout; }
+		inline std::chrono::milliseconds main_time_ms() const { return this->_main_time_ms; }
+		inline std::chrono::milliseconds byoyomi_ms() const { return this->_byoyomi_ms; }
+		inline int32_t byoyomi_stones() const { return this->_byoyomi_stones; }
+		inline int32_t byoyomi_stones_left() const { return this->_byoyomi_stones_left; }
+		inline std::chrono::milliseconds increment_ms() const { return this->_increment_ms; }
+		inline bool is_ticking() const { return this->_is_ticking; }
+		inline bool timeout() const { return this->_timeout; }
 		void start();
 		void stop();
 		void reset();
 		inline void restart() { reset(); start(); }
+		
+		template<bool INCLUDE_BYOYOMI>
 		std::chrono::milliseconds time_left_ms();
+
+		std::chrono::milliseconds byoyomi_left_ms();
 	};
 }
